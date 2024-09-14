c98e56b038f6b77f1e2b5209becad482079e6b49
devindudeman <miscellaneous1@gmail.com>
2024-09-13 21:51:21 -0700

    small changes

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ const AppWrapper = styled.div`
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <GlobalStyle />
       <AppWrapper>
         <Routes>
